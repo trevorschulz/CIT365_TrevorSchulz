7be2908a0f3d32c5ab3490fc655c8f4b9a371930
Trevor <trevorschulz@users.noreply.github.com>
2020-01-11 20:14:02 -0700

    Start of first console program, followed the tutorial video. Program just asks for favorite color

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is your favorite Color?");
+
+            var respone = Console.ReadLine();
+
+            Console.Write("You said your favorite color was " + respone + ".");
+
+            Console.ReadLine();
+        }
+    }
+}
