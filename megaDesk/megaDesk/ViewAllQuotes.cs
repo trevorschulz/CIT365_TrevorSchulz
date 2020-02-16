19033874a93634a95b037babf7b7c45ca57f8b70
Trevor <trevorschulz@users.noreply.github.com>
2020-02-15 21:47:03 -0700

    Adding local repo folders

---
+++ megaDesk/megaDesk/ViewAllQuotes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace megaDesk
+{
+    public partial class ViewAllQuotes : Form
+    {
+        public ViewAllQuotes()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
